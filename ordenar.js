155accd4f1e47da8075f56dea706105de0498a9e
Alexander <alexader5003@gmail.com>
2022-01-13 23:42:38 -0600

    Ordenado con for

--- ordenar.js
+++ ordenar.js
@@ -1,17 +1,34 @@
 let desordenado = [6,8,5,4,7,9,2,3,1];
 let ordenado=[];
-let comaparaciones=0;
+let comparaciones=0;
+
+//
+
+let tamaño = desordenado.length;
 
 
-while (ordenado.length<desordenado.length) {
+/* while (ordenado.length<tamaño) {
     for (const comparador of desordenado) {
         for (const comparado of desordenado) {
             if (comparador>comparado) {
-                comaparaciones=comaparaciones+1;
+                comparaciones=comparaciones+1;
+                console.log('1');
             }
         }
+        console.log('paso');
+        if (comparaciones==0) {
+            ordenado.push(comparador);
+            console.log('g');
+            for (const i in desordenado) {
+                if (desordenado[i]==comparador) {
+                    desordenado.splice(i,1);
+                }
+            }
+        }
+        comparaciones=0;
     }
-    if (comaparaciones==1) {
-        ordenado.push;
-    }
-}
+} */
+
+desordenado.sort(function (a,b) {
+    return a-b;
+});
