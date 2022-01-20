607aa48634396fbd60d64fa80d96f2a82f851f6f
Alexander <alexader5003@gmail.com>
2022-01-19 23:35:12 -0600

    solo eso logre subir hoy, una desepcion total

--- moda.js
+++ moda.js
@@ -1,21 +1,28 @@
-let lista1 = [1,2,3,1,2,3,4,2,2,2,1];
+let lista1 = [];
 let lista1Count = {};
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] +=1;
-        }else{
-            lista1Count[elemento]=1;
+let moda;
+
+
+function encontrarModa(dato) {
+    lista1=dato;
+    //Primer paso
+    lista1.map(
+        function (elemento) {
+            if (lista1Count[elemento]) {
+                lista1Count[elemento] +=1;
+            }else{
+                lista1Count[elemento]=1;
+            }
         }
-    }
-);
-
-
-let listaArray = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB) {
-        return elementoA[1]-elementoB[1];
-    }
-)
-
-let moda = listaArray[listaArray.length-1];
+    );
+    //Paso final
+    let listaArray = Object.entries(lista1Count).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1]-elementoB[1];
+        }
+    )
+    
+    moda = listaArray[listaArray.length-1];
+    console.log(moda);
+}
